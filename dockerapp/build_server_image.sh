6431488b41ffbbd4aa8a17669c115ab4ef6e1986
Evgeny Koshelev <eko2000@gmail.com>
2024-01-14 19:41:04 +0100

    Build and push Docker image to Google cloud artifacts storage

--- dockerapp/build_server_image.sh
+++ dockerapp/build_server_image.sh
@@ -1,9 +1,9 @@
 echo "Buiding docker image..."
 
-if [ ! -f Dockerfile ]
+if [ "$(basename $(pwd))" != "dockerapp" ]
 then
-     echo 'Please, run in the dockerapp folder, not GIT root.'
-     exit
+  echo "The build image should be run from \"dockerapp\" directory."
+  exit 1
 fi
 
 if [ -f package.json ]
@@ -17,8 +17,20 @@ cp -R ../src ./
 cp -R ../public ./
 cp ../package.json ./
 
-docker build . -t ibnazer/cleanappapp:1.6
-# docker push ibnazer/cleanappapp
+. .version
+
+CLOUD_REGION="us-central1"
+PROJECT_NAME="cleanup-mysql-v2"
+DOCKER_IMAGE="cleanapp-docker-repo/cleanapp-web-image"
+DOCKER_TAG="${CLOUD_REGION}-docker.pkg.dev/${PROJECT_NAME}/${DOCKER_IMAGE}"
+
+echo "Building and pushing docker image..."
+gcloud builds submit \
+  --region=${CLOUD_REGION} \
+  --tag ${DOCKER_TAG}:${BUILD_VERSION}
+
+echo "Tagging Docker image as latest..."
+gcloud artifacts docker tags add ${DOCKER_TAG}:${BUILD_VERSION} ${DOCKER_TAG}:latest
 
 rm -f -d -R src
 rm -f -d -R public
